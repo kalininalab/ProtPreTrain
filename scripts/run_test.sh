cb51ab641bb34656f0269b755bea8b4d69c2b7c5
Ilya Senatorov <il.senatorov@protonmail.com>
2022-08-30 23:38:44 +0200

    updated script

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
-#SBATCH --account=<budget>
-#SBATCH --nodes=4
+#SBATCH --account=hai_pre_prot
+#SBATCH --nodes=1
 #SBATCH --ntasks=16
 #SBATCH --ntasks-per-node=4
 #SBATCH --output=gpu-out.%j
 #SBATCH --error=gpu-err.%j
 #SBATCH --time=00:15:00
-#SBATCH --partition=<partition>
+#SBATCH --partition=booster
 #SBATCH --gres=gpu:4
 
-conda activate torch
-srun python train.py --config config/small_config.yaml
+conda activate protpretrain
+srun python train.py --config config/small_config.yaml --data.root $PROJECT_hai_pre_prot/data
