123f957423c1fbf93dd7a41e78b79f877b506177
Ilya Senatorov <senatorov1@jwlogin22.juwels>
2022-08-31 15:47:06 +0200

    updates

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 
 #SBATCH --account=hai_pre_prot
-#SBATCH --nodes=1
-#SBATCH --gres=gpu:4
-#SBATCH --ntasks-per-node=16
+#SBATCH --job-name=test
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
 #SBATCH --mem=0
-#SBATCH --time=00:20:00
+#SBATCH --time=0-00:20:00
 #SBATCH --partition=booster
 
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
 conda activate protpretrain
 srun python train.py --config config/small_config.yaml --data.root $PROJECT_hai_pre_prot/data
--- train.py
+++ train.py
@@ -3,13 +3,14 @@ from pytorch_lightning.cli import LightningCLI
 from protpretrain.data import AlphaFoldDataModule
 from protpretrain.models.denoise import DenoiseModel
 from protpretrain.utils.cli import namespace_to_dict
+import wandb
 
-if __name__ == "__main__":
-    cli = LightningCLI(
-        model_class=DenoiseModel,
-        datamodule_class=AlphaFoldDataModule,
-        run=False,
-        save_config_callback=None,
-    )
-    cli.trainer.logger.experiment.config.update(namespace_to_dict(cli.config))
-    cli.trainer.fit(cli.model, cli.datamodule)
+wandb.init(settings=wandb.Settings(start_method='fork'), project="pretrain_alphafold", name="test", mode="offline")
+cli = LightningCLI(
+    model_class=DenoiseModel,
+    datamodule_class=AlphaFoldDataModule,
+    run=False,
+    save_config_callback=None,
+)
+cli.trainer.logger.experiment.config.update(namespace_to_dict(cli.config))
+cli.trainer.fit(cli.model, cli.datamodule)
