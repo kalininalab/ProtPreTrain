be2d2eee727bbe68e7d20d619f95d5f6bfaacdeb
Ilya Senatorov <il.senatorov@protonmail.com>
2022-08-31 10:50:21 +0200

    updated script

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -l
 
+#SBATCH --account=hai_pre_prot
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=16
 #SBATCH --mem=0
-#SBATCH --time=0-02:00:00
+#SBATCH --time=00:20:00
+#SBATCH --partition=booster
 
-# module load NCCL/2.4.7-1-cuda.10.0
 
-
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 conda activate protpretrain
 srun python train.py --config config/small_config.yaml --data.root $PROJECT_hai_pre_prot/data
