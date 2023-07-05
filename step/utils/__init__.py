718a381d91fb8a21812daa41a0ae93cd8540b926
Ilya Senatorov <il.senatorov@protonmail.com>
2023-07-05 14:44:11 +0200

    added custom lr scheduler

--- step/utils/__init__.py
+++ step/utils/__init__.py
@@ -1,3 +1,4 @@
 from .cli import IterDict, add_arg_prefix, read_config, recursive_apply, remove_arg_prefix, write_config
 from .math import minmax_normalise, split_random, to_prob
+from .optim import WarmUpCosineLR
 from .vis import plot_aa_tsne, plot_confmat, plot_loss_count_dist, plot_node_embeddings, plot_noise_pred
---
+++ step/utils/optim.py
@@ -0,0 +1,53 @@
+import torch
+
+
+class WarmUpCosineLR(torch.optim.lr_scheduler._LRScheduler):
+    """
+    Warmup + Cosine LR. Uses number of steps rather than number of epochs.
+    """
+
+    def __init__(
+        self,
+        optimizer: torch.optim.Optimizer,
+        warmup_steps: int = 100,
+        start_lr: float = 1.0e-5,
+        max_lr: float = 1.0e-4,
+        min_lr: float = 1.0e-7,
+        cycle_len: int = 1000,
+    ):
+        self.warmup_steps = warmup_steps
+        self.start_lr = start_lr
+        self.max_lr = max_lr
+        self.min_lr = min_lr
+        self.cycle_len = cycle_len
+        self.step_count: int = 0
+        super().__init__(optimizer)
+
+    def _get_warmup_lr(self):
+        lr = self.start_lr + (self.max_lr - self.start_lr) * self.step_count / self.warmup_steps
+        print(lr)
+        return lr
+
+    def _get_cosine_lr(self):
+        lr = (
+            self.min_lr
+            + (self.max_lr - self.min_lr)
+            * (1 + math.cos(math.pi * (self.step_count - self.warmup_steps) / self.cycle_len))
+            / 2
+        )
+        print(lr)
+        return lr
+
+    def get_lr(self):
+        """Calculates lr. Linear before `warmup_steps`, cosine between `max_lr` and `min_lr` after."""
+        if self.step_count <= self.warmup_steps:
+            return [self._get_warmup_lr() for _ in self.optimizer.param_groups]
+        # elif self.step_count >= self.warmup_steps + self.cycle_len:
+        #     return [self.min_lr for _ in self.optimizer.param_groups]
+        else:
+            return [self._get_cosine_lr() for _ in self.optimizer.param_groups]
+
+    def step(self, epoch=None):
+        """Updates number of steps, then same stuff."""
+        self.step_count += 1
+        super().step(epoch)
